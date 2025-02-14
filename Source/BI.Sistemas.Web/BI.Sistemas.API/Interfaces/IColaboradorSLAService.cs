2a86e36ce5c4fb8a669be0b17927b5b2b2c6910e
PetronioFaleixo <79844325+PetronioFaleixo@users.noreply.github.com>
2025-02-14 12:54:57 -0300

    Atualizando .gitignore para ignorar arquivos do Visual Studio e avaliação de projeto

--- Source/BI.Sistemas.Web/BI.Sistemas.API/Interfaces/IColaboradorSLAService.cs
+++
@@ -1,6 +0,0 @@
-﻿using BI.Sistemas.API.View;
-
-public interface IColaboradorSLAService 
-{
-    ColaboradorSLADashboardView GetColaboradorDashboard(string id);
-}
--- Source/BI.Sistemas.Web/BI.Sistemas.API/Interfaces/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using BI.Sistemas.API.View;
-
-namespace BI.Sistemas.API.Interfaces
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(EnviarEmailDados dados);
-    }
-}
